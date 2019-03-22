07223483af7f334368fff49976253cfbcf9d0f42
DavidFarnick <dfarnick2@gmail.com>
2019-03-22 07:52:34 -0500

    update

--- public/app.js
+++ public/app.js
@@ -31,7 +31,7 @@ $(document).on("click", ".addnote", function() {
     .done(function(data) {
       console.log(data);
       // The title of the article
-      $("#notes").append("<h4>" + data.title + "</h4>");
+      $("#notes").append("<h6>" + data.title + "</h6>");
       // An input to enter a new title
       $("#notes").append("<input id='titleinput' name='title' >");
       // A textarea to add a new note body
